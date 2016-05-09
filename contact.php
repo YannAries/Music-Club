88fcd92e902473d07d544f052b6a273e15de11ee
hodd <hodd@gmx.com>
2016-05-09 15:43:10 +0200

    llo

--- contact.php
+++ contact.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once 'partials/header.php';
 
 // On réceptionne les données du formulaire
@@ -10,8 +10,8 @@ $status = false;
 $errors = array();
 
 // Le formulaire a été soumis, on a appuyé sur le bouton "Envoyer"
-if (!empty($_POST)) { 
-	
+if (!empty($_POST)) {
+
 	// On vérifie les erreurs possibles
 	if (empty($nom)) {
 		$errors['nom'] = 'Veuillez renseigner votre nom';
@@ -29,10 +29,10 @@ if (!empty($_POST)) {
 	}
 
 	//debug($errors);
-	
+
 	// Aucune erreur dans le formulaire, tous les champs ont été saisis correctement
 	if (empty($errors)) {
-		$query = $db->prepare('INSERT INTO mail SET nom = :nom, email = :email, sujet = :sujet, message = :message, date = NOW()');
+		$query = $db->prepare('INSERT INTO contact SET nom = :nom, email = :email, sujet = :sujet, message = :message, date = NOW()');
 		// Pour chacune des variables précédées d'un : on doit faire un bindValue pour passer la valeur à la requête
 		$query->bindValue(':nom', $nom, PDO::PARAM_STR);
 		$query->bindValue(':email', $email, PDO::PARAM_STR);
@@ -46,7 +46,7 @@ if (!empty($_POST)) {
 		$insert_id = $db->lastInsertId();
 		if (!empty($insert_id)) {
 			$status = true;
-		} 
+		}
 		else {
 			$errors = array('db_error' => 'Erreur interne, merci de réessayer ultérieurement!');
 		}
@@ -65,8 +65,8 @@ $result = array(
 				<div class="container">
 					<div class="contact-top">
 						<h3>Nous contacter</h3>
-					</div>		
-		
+					</div>
+
 					<div class="contact-grids">
 						<div class="contact-form">
 							<form>
@@ -94,7 +94,7 @@ $result = array(
 							</form>
 						</div>
 						<div class="address">
-					
+
 					<div class="col-md-4 address-more">
 						<h4>Adresse</h4>
 						<div class="address-grid">
@@ -114,7 +114,7 @@ $result = array(
 						</div>
 						</div>
 							<div class="col-md-8 map">
-							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d325516.3770455822!2d30.532690549999998!3d50.40203550000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cf4ee15a4505%3A0x764931d2170146fe!2sKyiv%2C+Ukraine!5e0!3m2!1sen!2sin!4v1435638348391" allowfullscreen=""></iframe>				
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d325516.3770455822!2d30.532690549999998!3d50.40203550000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cf4ee15a4505%3A0x764931d2170146fe!2sKyiv%2C+Ukraine!5e0!3m2!1sen!2sin!4v1435638348391" allowfullscreen=""></iframe>
 						</div>
 						<div class="clearfix"> </div>
 						</div>
